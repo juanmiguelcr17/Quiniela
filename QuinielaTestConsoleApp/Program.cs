c3e4418e3c0244d1764b88a2895df9388619156d
juanmiguelcr17 <jumicarod_93@hotmail.com>
2017-01-27 13:50:35 -0600

    Se añade proyecto para pruebas por consola.

---
+++ QuinielaTestConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using UtilsLibrary.Serialization;
+
+namespace QuinielaTestConsoleApp
+{
+    class Program
+    {
+        static Writter wr = new Writter();
+        static Test test = new Test();
+        static void Main(string[] args)
+        {
+            test.crearLiga();
+            writeToFile("Liga", serialize(test.ligaMX));
+            test.CrearTemporada();
+            writeToFile("Temporada", serialize(test.temporada));
+            test.CrearEquipos();
+            test.CrearJornada();
+            writeToFile("Jornada 1", serialize(test.semana1));
+            test.CrearQuiniela();
+            writeToFile("Quiniela J1", serialize(test.Quiniela1));
+            test.CrearPrediccion();
+            writeToFile("Prediccion J1", serialize(test.Prediccion1));
+            test.SetResultsOfWeek();
+            test.VerifyResults();
+            writeToFile("Resultados Jornada 1", serialize(test.semana1));
+        }
+        static string serialize(object data)
+        {
+            return wr.SerializarAJson(data);
+        }
+        static void writeToFile(string fileName, string data)
+        {
+            string path =string.Format(@"{0}\{1}.{2}", System.Configuration.ConfigurationManager.AppSettings["Path"], fileName, "json");
+            File.WriteAllText(path, data);
+        }
+    }
+}
---
+++ QuinielaTestConsoleApp/Test.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using QuinielaLibrary;
+using QuinielaLibrary.Entities;
+using QuinielaLibrary.Pools;
+
+namespace QuinielaTestConsoleApp
+{
+    public class Test
+    {
+        Actions act = new Actions();
+        string[] equipos = new string[] { "Veracruz", "Queretaro", "Cruz Azul", "Necaxa", "Tigres", "Santos", "Morelia", "Tijuana", "Leon", "Pachuca", "Chivas", "Pumas", "Toluca", "Atlas", "Puebla", "Monterrey", "Jaguares", "America" };
+        string[] abb = new string[] { "Ver", "Que", "Caz", "Nec", "Tig", "San", "Mor", "Tij", "Leo", "Pac", "Chi", "UNAM", "Tol", "Atl", "Pue", "Mty", "Jag", "Ame" };
+        int[] colors = new int[] { -65536, -16777216, -16776961, -16181, -329006, -16744448, -23296, -65536, -16744448, -12042869, -65536, -10496, -65536, -65536, -16728065, -16777077, -16744448, -256 };
+        List<Team> teams = new List<Team>();
+        internal Season temporada { get; set; }
+        internal League ligaMX { get; set; }
+        internal List<Game> Partidos { get; set; }
+        internal Week semana1 { get; set; }
+        internal Pool Quiniela1 { get; set; }
+        internal Prediction Prediccion1 { get; set; }
+        internal int aciertos { get; set; }
+
+        internal void crearLiga()
+        {
+            ligaMX = act.CreateLeague(Guid.NewGuid(), "Liga MX", "México");
+        }
+
+        internal void CrearTemporada()
+        {
+            temporada = act.CreateSeason(Guid.NewGuid(), "Clausura 2017", DateTime.Now, DateTime.Now, DateTime.Now, ligaMX, 17);
+        }
+
+        internal void CrearEquipos()
+        {
+            for (int i = 0; i <= 17; i++)
+            {
+                teams.Add(new Team(Guid.NewGuid(), abb[i], equipos[i], abb[i], "", "", "", colors[i]));
+            }
+            act.AddTeams(teams, temporada);
+        }
+
+        internal void CrearJornada()
+        {
+            Partidos = new List<Game>();
+            for (int i = 0; i <= 17; i += 2)
+            {
+                Partidos.Add(act.CreateGame(Guid.NewGuid(), teams.ElementAt(i), teams.ElementAt(i + 1), DateTime.Now));
+            }
+            semana1 = act.CreateWeek(Guid.NewGuid(), 1, Partidos);
+        }
+
+        internal void CrearQuiniela()
+        {
+            Quiniela1 = act.CreatePool(Guid.NewGuid(), DateTime.Now, DateTime.Now, semana1.Games);
+        }
+        
+        internal void CrearPrediccion()
+        {
+            Prediccion1 = new Prediction();
+            Prediccion1.Id = Guid.NewGuid();
+            Prediccion1.User = new User();
+            Prediccion1.Group = new Group();
+            Prediccion1.Predictions = new List<GamePrediction>();
+            foreach (var game in Quiniela1.Games)
+            {
+                Prediccion1.Predictions.Add(new GamePrediction(game, QuinielaLibrary.Catalogs.Enumerations.Result.Local));
+            }
+        }
+
+        internal void SetResultsOfWeek()
+        {
+            Dictionary<Guid, QuinielaLibrary.Catalogs.Enumerations.Result> Results = new Dictionary<Guid, QuinielaLibrary.Catalogs.Enumerations.Result>();
+
+            Results.Add(semana1.Games.ElementAt(0).Id, QuinielaLibrary.Catalogs.Enumerations.Result.Local);
+            Results.Add(semana1.Games.ElementAt(1).Id, QuinielaLibrary.Catalogs.Enumerations.Result.Local);
+            Results.Add(semana1.Games.ElementAt(2).Id, QuinielaLibrary.Catalogs.Enumerations.Result.Empate);
+            Results.Add(semana1.Games.ElementAt(3).Id, QuinielaLibrary.Catalogs.Enumerations.Result.Local);
+            Results.Add(semana1.Games.ElementAt(4).Id, QuinielaLibrary.Catalogs.Enumerations.Result.Vistante);
+            Results.Add(semana1.Games.ElementAt(5).Id, QuinielaLibrary.Catalogs.Enumerations.Result.Local);
+            Results.Add(semana1.Games.ElementAt(6).Id, QuinielaLibrary.Catalogs.Enumerations.Result.Local);
+            Results.Add(semana1.Games.ElementAt(7).Id, QuinielaLibrary.Catalogs.Enumerations.Result.Vistante);
+
+            act.SetResultsOfWeek(semana1.Games, Results);
+        }
+
+        internal void VerifyResults()
+        {
+            aciertos = act.GetResultsAgainstPredictions(semana1.Games, Prediccion1.Predictions);
+        }
+    }
+}
+
