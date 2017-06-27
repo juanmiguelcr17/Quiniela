a72e54151e766a67274791e3f2b4f1f89f1ec747
juanmiguelcr17 <jumicarod_93@hotmail.com>
2017-06-27 12:36:10 -0500

    Custom Pages para errores generados

---
+++ QuinielaMVC4/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuinielaMVC4.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HandleError]
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+
+    }
+}
